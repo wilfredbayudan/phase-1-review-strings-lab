d75e12008358c26b65fe27589567a0935a1bd4c1
Wilfred <wilfredbayudan@gmail.com>
2021-08-09 23:34:05 -1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser = "Wilfred";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
